88e06e0dc20ba7ecd4c8421fb8dbc80eb4dd5983
lijiaren <lijiaren@kyee.com.cn>
2016-02-28 11:29:36 +0800

    提交框架

---
+++ src/main/java/com/R/NewWorldStartApplication.java
@@ -0,0 +1,12 @@
+package com.R;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWorldStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewWorldStartApplication.class, args);
+	}
+}
